00bff4ee1bc6f0ebe244cad73fb2a1faf4af4049
Amador Hinojosa Gálvez (IZV) <jhingal3010@ieszaidinvergeles.org>
2025-11-30 18:57:01 +0100

    TRANSLATED apache2-provision comments

--- vagrant.student/apache2-provision.sh
+++ vagrant.student/apache2-provision.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo "==> Copiando configuraciones de Apache..."
+echo "==> Copying apache config files..."
 
 cp -v /vagrant/files/mercurio.olimpo.test/apache2.conf /etc/apache2/apache2.conf 2>/dev/null
 
 cp -v /vagrant/files/mercurio.olimpo.test/apolo.olimpo.test.conf  /etc/apache2/sites-available/
 cp -v /vagrant/files/mercurio.olimpo.test/atenea.olimpo.test.conf /etc/apache2/sites-available/
 
-echo "==> Habilitando sitios virtuales..."
+echo "==> Enabling virtual sites..."
 
 a2ensite apolo.olimpo.test.conf
 a2ensite atenea.olimpo.test.conf
 
-echo "==> Deshabilitando sitio por defecto..."
+echo "==> Disabling defult sites..."
 a2dissite 000-default.conf
 
-echo "==> Comprobando sintaxis de Apache..."
+echo "==> Checking apache sintax..."
 apachectl configtest
 
-echo "==> Reiniciando Apache..."
+echo "==> Restarting Apache..."
 systemctl restart apache2
 systemctl enable apache2
